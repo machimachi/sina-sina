1e784eb76c80c12ad4f8c95155c34e4b08d95ceb
machida <machida@192.168.11.63>
2016-02-14 09:03:13 +0900

    feed plus

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'rss'
 get '/' do
  #@hello = "こんにちは！"
  @rss = RSS::Parser.parse("http://mmaplanet.jp/feed")
+ @rss_mmaplanet_report = RSS::Parser.parse("http://mmaplanet.jp/report/feed")
  erb :index 
  #'HerokuでSinatraを使ってHello world!'
 end
