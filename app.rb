4aad3e6c45a7284d06ebe13c3732637abc76679e
machida <machida@192.168.11.63>
2016-02-14 20:27:40 +0900

    add bootstrap

--- app.rb
+++ app.rb
@@ -1,11 +1,24 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
 require 'rss'
 
 get '/' do
- #@hello = "こんにちは！"
+    erb :index
+end
+
+get '/page2' do
+    erb :page2
+end
+
+get '/mmantena' do
  @rss = RSS::Parser.parse("http://mmaplanet.jp/feed")
  @rss_mmaplanet_report = RSS::Parser.parse("http://mmaplanet.jp/report/feed")
- erb :index 
- #'HerokuでSinatraを使ってHello world!'
+ erb :mma
+end
+
+get '/hatena-it' do
+ @rss = RSS::Parser.parse("http://mmaplanet.jp/feed")
+ @rss_mmaplanet_report = RSS::Parser.parse("http://mmaplanet.jp/report/feed")
+ erb :mma
 end
